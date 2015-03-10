a5a6a8310c36e56b97b458d008c59cb41acc6547
deniznida <denizunat@hotmail.com>
2015-03-10 08:01:04 -0400

    done

--- db/schema.rb
+++ db/schema.rb
@@ -13,14 +13,14 @@
 
 ActiveRecord::Schema.define(version: 20140513204421) do
 
-  create_table "post_tags", force: true do |t|
+  create_table "post_tags", force: :cascade do |t|
     t.integer  "post_id"
     t.integer  "tag_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "posts", force: true do |t|
+  create_table "posts", force: :cascade do |t|
     t.string   "name"
     t.text     "content"
     t.integer  "user_id"
@@ -28,13 +28,13 @@ ActiveRecord::Schema.define(version: 20140513204421) do
     t.datetime "updated_at"
   end
 
-  create_table "tags", force: true do |t|
+  create_table "tags", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
